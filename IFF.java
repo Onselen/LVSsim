3359c77fa13f1022837e26382dbf4b554c84fc33
Rico van Onselen <ricovanonselen@gmail.com>
2024-03-25 21:44:32 +0100

    Add IFF and Radar classes

---
+++ IFF.java
@@ -0,0 +1,13 @@
+public class IFF {
+
+    public IFF() {}
+
+    public boolean friendOrFoe(String radarData){
+        // Identify if scan has detected a friend or foe
+
+        String[] entries = radarData.split(";");
+
+        return true;
+    }
+    
+}
---
+++ Radar.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Radar {
+    File radarDataFile;
+    Scanner fileScanner;
+
+    public Radar(String passedRadarDataFilename) {
+        radarDataFile = new File(passedRadarDataFilename);
+        try{
+            fileScanner = new Scanner(radarDataFile);}
+        catch (FileNotFoundException e){
+            System.err.println("Radar data file not found");
+        }
+    }
+
+    String scanForThreats() {
+        return fileScanner.nextLine();
+    }
+}
