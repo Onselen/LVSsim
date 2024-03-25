c4cff40bb8718a8932762255295a82772309dc64
Rico van Onselen <ricovanonselen@gmail.com>
2024-03-25 21:48:25 +0100

    Add simulation setup

--- Main.java
+++ Main.java
@@ -10,6 +10,11 @@ public class Main {
         // One unit can consist of one or more of these components
         // 
 
+        // Parameter settings
+        String radarDataFilename = "radar_data.csv";  // Filename of file that contains the radardata
 
+        // Instantiate components of simulation
+        Radar radar1 = new Radar(radarDataFilename);
+        IFF iff1 = new IFF();
     }
 }
