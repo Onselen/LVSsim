6a1181dafd1d4a4511b85ce89ab6ef51b8b16dc6
Rico van Onselen <ricovanonselen@gmail.com>
2024-03-25 20:35:12 +0100

    Add comment main()

--- Main.java
+++ Main.java
@@ -2,5 +2,14 @@
 public class Main {
     public static void main(String[] args){
 
+        // This program will simulate three components:
+        // - Radar
+        // - Identification Friend or Foe (IFF
+        // - Missile launcher
+        //
+        // One unit can consist of one or more of these components
+        // 
+
+
     }
 }
