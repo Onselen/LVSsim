191d07f767810f9ef9867c2c2ff5b4d46c40b4e0
Rico van Onselen <ricovanonselen@gmail.com>
2024-03-25 21:51:28 +0100

    Add main simulation loop

--- Main.java
+++ Main.java
@@ -11,10 +11,28 @@ public class Main {
         // 
 
         // Parameter settings
+        int noScans = 20;                             // Amount of scans the simulation takes
+        long helpTime = 0;
         String radarDataFilename = "radar_data.csv";  // Filename of file that contains the radardata
 
         // Instantiate components of simulation
         Radar radar1 = new Radar(radarDataFilename);
         IFF iff1 = new IFF();
+
+        // Simulation will start
+        int i = 0;
+        while (i < noScans) {
+            helpTime = 0;
+
+            // Scan for threats
+            String radarData = radar1.scanForThreats();
+
+            // Identify friend or foe
+            iff1.friendOrFoe(radarData);
+
+            // Launch missile if necessary
+
+            i++;
+        }
     }
 }
